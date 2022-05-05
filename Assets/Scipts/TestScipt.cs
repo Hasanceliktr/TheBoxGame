ff0b08a1590a29d3627dbf377eb38023e7e024ae
Hasan Çelik <hasancelikdeveloper@gmail.com>
2022-05-05 23:43:47 +0300

    Test yapıldı.

--- Assets/Scipts/TestScipt.cs
+++ Assets/Scipts/TestScipt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestSciptsTwo : MonoBehaviour
+public class TestScipt : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
