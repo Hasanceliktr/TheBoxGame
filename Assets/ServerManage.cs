b690b02d27924ad141172336629b8fbff70db58e
Hasan Çelik <hasancelikdeveloper@gmail.com>
2022-05-11 20:13:16 +0300

    Server ayarları yapıldı.

--- Assets/ServerManage.cs
+++ Assets/ServerManage.cs
@@ -2,18 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class ServerManage : MonoBehaviour
+namespace ServerManage
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ServerManage : MonoBehaviourPunCallbacks
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Server is connected.");
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            Debug.Log("Lobby is connected.");
+
+        }
+
+        public void RandomJoined()
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public void CreateRoomJoined()
+        {
+            string roomName = Random.Range(0, 99999).ToString();
+            PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions { MaxPlayers = 2, IsOpen = true, IsVisible = true }, TypedLobby.Default);
+        }
+
+        public override void OnLeftRoom()
+        {
+            //Odadan çýkýldýðýnda yapýlmasý gerekenler.
+        }
+
+        public override void OnLeftLobby()
+        {
+            //Lobiden çýkýldýðýnda yapýlmasý gerekenler.
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            //Odadan oyuncu ayrýlýnca yapýlacak iþlemler.
+
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            //Oda oluþturulurken olabilecek hatalar.
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            //Random odaya katýlýnamazsa alýnacak hata.
+        }
+
     }
 }
+
